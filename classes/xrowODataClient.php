94d7fddc32ebdb5d3b23bad2cc34e1ee32f2afca
xinyuexrow <wang@xrow.de>
2013-08-20 19:28:53 +0200

    code modification

--- classes/xrowODataClient.php
+++ classes/xrowODataClient.php
@@ -2,24 +2,24 @@
 
 class xrowODataClient
 {
-	const DIR = 'var/cache/xrowodata';
-    function factory( $uri )
+    const DIR = 'var/cache/xrowodata';
+    function factory( $uri,$entities )
     {
-    	if (!is_dir(self::DIR))
-    	{
-    		eZDir::mkdir( self::DIR , false, true );
-    	}
-    	
+        if (!is_dir(self::DIR))
+        {
+            eZDir::mkdir( self::DIR , false, true );
+        }
+        
         set_include_path( get_include_path() . PATH_SEPARATOR . '.' . DIRECTORY_SEPARATOR . 'extension' . DIRECTORY_SEPARATOR . 'xrowodata' . DIRECTORY_SEPARATOR . 'src' .DIRECTORY_SEPARATOR . 'OData_PHP_SDK' . DIRECTORY_SEPARATOR . 'framework' );
         $GLOBALS['ODataphp_path'] = './extension/xrowodata/src/OData_PHP_SDK/framework';
         $argv = array( 
-            'extension\xrowodata\bin\test.php' , 
-            '/uri=http://admin:publish@trunk.example.com/api/odata/v1/ezpublish.svc' , 
-            '/out=var/cache/xrowodata' , 
+            '' , 
+            '/uri='.$uri , 
+            '/out=var/cache/xrowodata/'.$entities , 
             '/ups=no' 
         );
         require_once 'PHPDataSvcUtil.php';
-        require_once self::DIR . '/eZPublishEntities.php';
+        require_once self::DIR . '/' . $entities;
         
         $proxy = new eZPublishEntities();
         return $proxy;
